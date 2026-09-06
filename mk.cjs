a38a7891fb4115258aae15557953b7241891b305
Jeremy <jeremyyboahdarko@gmail.com>
2026-09-06 14:44:21 +0200

    chore: remove stray codemod helper

--- mk.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const lines = fs.readFileSync("public/clientSocialSurfaces.js", "utf8").split(/\r?\n/);
-const a = [...lines.slice(0, 22), ...lines.slice(63, 77)];
-fs.writeFileSync("public/clientDupTest.js", a.join("\n") + "\n");
-console.log("---- file:");
-console.log(a.map((x) => (x.length > 90 ? x.slice(0, 90) + "…" : x)).join("\n"));
